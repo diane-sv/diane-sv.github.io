21e7564c1ab5bf2bacd4f963140c4db8bee87f2f
Diane Villaroman <dsvillaroman@gmail.com>
2018-06-01 11:28:25 -0700

    Create scrollDown.js

---
+++ js/scrollDown.js
@@ -0,0 +1,27 @@
+ $(function() {
+    $('#scrolldown0').click (function() {
+      $('html, body').animate({scrollTop: $('#section1').offset().top }, 'slow');
+      return false;
+    });
+  });
+
+ $(function() {
+    $('#scrolldown1').click (function() {
+      $('html, body').animate({scrollTop: $('#section2').offset().top }, 'slow');
+      return false;
+    });
+  });
+
+ $(function() {
+    $('#scrolldown2').click (function() {
+      $('html, body').animate({scrollTop: $('#section3').offset().top }, 'slow');
+      return false;
+    });
+  });
+
+ $(function() {
+    $('#scrolldown3').click (function() {
+      $('html, body').animate({scrollTop: $('#section4').offset().top }, 'slow');
+      return false;
+    });
+  });
